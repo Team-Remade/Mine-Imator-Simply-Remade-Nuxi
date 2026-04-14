daaec0b9283f91e9d9d74c26066d37f918c77f64
Forest <wilsonforest99@gmail.com>
2026-04-14 11:38:39 -0400

    no

--- MineImatorSimplyRemadeNuxi/App.cs
+++ MineImatorSimplyRemadeNuxi/App.cs
@@ -54,10 +54,6 @@ public class App : Game
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-            Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
-
         MouseState currentMouse = Mouse.GetState();
 
         if (currentMouse.RightButton == ButtonState.Pressed)
